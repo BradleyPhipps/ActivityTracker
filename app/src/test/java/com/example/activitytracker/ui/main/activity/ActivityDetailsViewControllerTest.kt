38371aa6510505d49ec5acc01922fcbafdcf809d
Bradley Phipps <bradley.phipps@bbc.co.uk>
2022-02-14 23:05:20 +0000

    Update ActivityDetailsViewControllerTest.kt

--- app/src/test/java/com/example/activitytracker/ui/main/activity/ActivityDetailsViewControllerTest.kt
+++ app/src/test/java/com/example/activitytracker/ui/main/activity/ActivityDetailsViewControllerTest.kt
@@ -2,12 +2,14 @@ package com.example.activitytracker.ui.main.activity
 
 import com.example.MainCoroutineRule
 import com.example.activitytracker.models.ActivityCoreData
+import junit.framework.Assert.assertEquals
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mock
 import org.mockito.MockitoAnnotations
+import org.mockito.kotlin.any
 import org.mockito.kotlin.verify
 
 class MyActivitiesViewControllerTest {
@@ -52,20 +54,32 @@ class MyActivitiesViewControllerTest {
         `then view should be populated`()
     }
 
-    @Test
-    fun `when data is loaded ActivityData is invoked`()= mainCoroutineRule.runBlockingTest{
-        `given a viewController is initiated`()
-
-    }
 
     @Test
     fun `seekbarListener set on viewReady`()= mainCoroutineRule.runBlockingTest{
         `given a viewController is initiated`()
         `when onViewReady has been invoked`()
+        `then setProgressChangeListener is invoked in view`()
 
     }
-    
-//GIVEN
+
+    private fun `then setProgressChangeListener is invoked in view`() {
+        verify(mockView).setProgressOnChangeListener(any())
+    }
+
+    @Test
+    fun `followClickListener set on viewReady`()= mainCoroutineRule.runBlockingTest{
+        `given a viewController is initiated`()
+        `when onViewReady has been invoked`()
+        `then set FollowClickListener on viewReady `()
+
+    }
+
+    private fun `then set FollowClickListener on viewReady `() {
+        verify(mockView).setFollowButtonOnClickListener(any())
+    }
+
+    //GIVEN
     private fun `given a viewController is initiated`(){
         controller = ActivityDetailsViewController(mockView,mockViewModel)
         mockViewModel.activityCoreData = mockActivity
